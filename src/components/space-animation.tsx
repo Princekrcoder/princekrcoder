2ae5858cb2e94b00b12db32f1e1655f3614d04a4
prince kumar <princeshorts31@gmail.com>
2025-06-30 11:14:13 +0000

    add more stable stars and add shooting star shooting star

--- src/components/space-animation.tsx
+++ src/components/space-animation.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 
 export function SpaceAnimation() {
   return (
-    <div className="fixed inset-0 -z-10">
+    <div className="fixed inset-0 -z-10 overflow-hidden">
       <div id="stars"></div>
       <div id="stars2"></div>
       <div id="stars3"></div>
+      <div className="shooting-star"></div>
+      <div className="shooting-star"></div>
+      <div className="shooting-star"></div>
     </div>
   );
 }
