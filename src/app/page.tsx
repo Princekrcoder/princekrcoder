562d0d3a71534309353120e562b51ae5ae76bfe8
prince kumar <princeshorts31@gmail.com>
2025-06-30 23:06:16 +0000

    not showing skill

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,8 +17,8 @@ export default function Home() {
         <HeroSection />
         <AboutSection />
         <ServicesSection />
-        <SkillsSection />
         <ProjectsSection />
+        <SkillsSection />
       </main>
       <Footer />
       <TooltipProvider>
