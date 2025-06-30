59dd2d1e5066db0c84966157ddad221f9425fd9d
prince kumar <princeshorts31@gmail.com>
2025-06-30 13:02:49 +0000

    not looking properly message icon fix it

--- src/components/contact-icon.tsx
+++ src/components/contact-icon.tsx
@@ -14,12 +14,7 @@ export function ContactIcon({ className }: { className?: string }) {
           <stop offset="100%" stopColor="#52C7F4" />
         </linearGradient>
       </defs>
-      <path d="M21.92,2.65,3.44,19.92,9.85,13l3.54,3.54Z" />
-      <g opacity="0.7">
-        <path d="M9.85,13,3.44,19.92,l-.94,2.5,2.5-.94,5.43-5.43Z" />
-        <path d="M5.41,20.36a1.47,1.47,0,1,0,2.08,0,1.47,1.47,0,0,0-2.08,0Z" />
-        <path d="M2.46,23.33a1,1,0,1,0,1.41,0,1,1,0,0,0-1.41,0Z" />
-      </g>
+      <path d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z" />
     </svg>
   );
 }
