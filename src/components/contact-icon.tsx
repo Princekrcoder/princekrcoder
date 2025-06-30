5b4554f9e7c0581184f3b178a1b1e41adc75d98e
prince kumar <princeshorts31@gmail.com>
2025-06-30 12:57:10 +0000

    this message icon

--- src/components/contact-icon.tsx
+++ src/components/contact-icon.tsx
@@ -5,22 +5,10 @@ export function ContactIcon({ className }: { className?: string }) {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="1.8"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      fill="currentColor"
       className={className}
     >
-      <path d="M2.5 18.5 C 5 17, 19 17, 21.5 18.5" />
-      <path d="M4 18.5 L 4 10 C 4 9, 5 8, 6 8 L 13 8 C 14 8, 15 9, 15 10 L 15 18.5" />
-      <path d="M3 18.5 L 16 18.5" />
-      <circle cx="9.5" cy="10.5" r="0.5" fill="currentColor" strokeWidth="0" />
-      <path d="M18 18.5 L 18 14" />
-      <circle cx="18" cy="8.5" r="3" />
-      <path d="M15.5 8.5 A 3 3 0 0 1 20.5 8.5" />
-      <path d="M15.5 8.5 L 15.5 10.5" />
-      <path d="M15.5 11.5 A 1 1 0 0 1 14 10" />
+      <path d="M3.478 2.405a.75.75 0 00-.926.94l2.432 7.905H13.5a.75.75 0 010 1.5H4.984l-2.432 7.905a.75.75 0 00.926.94 60.519 60.519 0 0018.445-8.986.75.75 0 000-1.218A60.517 60.517 0 003.478 2.405z" />
     </svg>
   );
 }
